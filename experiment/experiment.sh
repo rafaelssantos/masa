611760b5264bae662636c98c8970cf6b9ad683d8
Rafael S Santos <rafael.silva.sts@gmail.com>
2019-12-03 14:48:27 -0300

    Experiments scripts

--- experiment/experiment.sh
+++ experiment/experiment.sh
@@ -12,28 +12,36 @@ input_data_path=""
 input_labels_path=""
 output_clustering_path=""
 
+
+
 init_paths(){
     input_data_path="/home/rafaelssantos/experiment/$1/$1.raw"
     input_labels_path="/home/rafaelssantos/experiment//$1/$1.info.txt"
     output_clustering_path="/home/rafaelssantos/experiment//$1/$1.clustered.csv"
 }
 
+
+
 # python3 $clustering_script_path -h
 
-# init_paths 'bucky-ball'
-# python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
 
-# init_paths 'foot'
-# python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
 
-# init_paths 'engine'
-# python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+exec_clustering(){
+    init_paths 'bucky-ball'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
 
-# init_paths 'visible-male-head'
-# python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+    init_paths 'foot'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
 
-# init_paths 'tooth'
-# python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+    init_paths 'engine'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
 
-init_paths 'tomato'
-python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+    init_paths 'visible-male-head'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+
+    init_paths 'tooth'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+
+    init_paths 'tomato'
+    python3 $clustering_script_path -i $input_data_path -m $n_features -n $n_instances -o $output_clustering_path
+}
